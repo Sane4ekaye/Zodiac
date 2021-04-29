dff053f8634cfe627e911ec0eaf1078b57835267
MistiJI <spam-mistbtv@yandex.ru>
2021-04-29 21:19:46 +0400

    добавил славика

--- app/src/main/java/com/example/horoscopes/MainActivity.kt
+++ app/src/main/java/com/example/horoscopes/MainActivity.kt
@@ -9,7 +9,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
-        val valera = "Леша" // AHAAHAHAHAHAAHAAHAHAHAAHGAHAHHAAHHAAH
+        val valera = "Леша" // AHAAHAHAHAHAAHAAHAHAHAAHGAHAHHAAHHAAHап
 
 //        ⠄⠄⠄⠄⠄⠄⠄⠄⠄⣠⠤⠖⠚⢉⣩⣭⡭⠛⠓⠲⠦⣄⡀⠄⠄⠄⠄⠄
 //        ⠄⠄⠄⠄⠄⠄⢀⡴⠋⠁⠄⠄⠊⠄⠄⠄⠄⠄⠄⠄⠄⠄⠉⠳⢦⡀⠄⠄
