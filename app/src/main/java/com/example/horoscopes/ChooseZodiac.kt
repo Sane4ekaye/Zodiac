033c9e1b77b2eed3ad2e5473551993171c747472
MistiJI <spam-mistbtv@yandex.ru>
2021-04-29 20:29:25 +0400

    test

--- app/src/main/java/com/example/horoscopes/ChooseZodiac.kt
+++ app/src/main/java/com/example/horoscopes/ChooseZodiac.kt
@@ -7,7 +7,7 @@ class ChooseZodiac : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_choose_zodiac)
-
+        //Ауе блин
         var mur = 12
     }
 }
