4a66e750de28d95633296f2e470de2ca3c72b1b7
MistiJI <spam-mistbtv@yandex.ru>
2021-04-30 01:09:11 +0400

    добавил + 1 активность с обратной связи, и расположил элементы в mainactivity

--- app/src/main/java/com/example/horoscopes/ChooseZodiac.kt
+++ app/src/main/java/com/example/horoscopes/ChooseZodiac.kt
@@ -1,27 +1,23 @@
 package com.example.horoscopes
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.Toast
 
 class ChooseZodiac : AppCompatActivity() {
 
-    var buttonTwins: Button? = null;
+    var btnOven: ImageView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_choose_zodiac)
-        var Mamasha_dasha = "Людмила"
-        var mur = 11
-        var mur2 = 124
-        var mur4 = 18
-
-        var kashtanchik: Boolean = false
-
-        var slava_gavrilov = "MARTISHKAAKAKA ludmila"
-        if(kashtanchik) println("Каштанчик ON!") else println("Каштанчик OFF!")
-
-        val syka_noviy_avtomat_eto_blyat_ebuchiy_dedov_kai_ya_ebal_oslov_mishyak = 123
+        btnOven = findViewById(R.id.buttonOven)
+        btnOven?.setOnClickListener{
+            var intent = Intent(this@ChooseZodiac, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
