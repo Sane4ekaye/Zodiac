fbb48cf2186274d623897328ffe0bfc72cf11e3d
chikibambsanya <BKyPeHblu456@yandex.ru>
2021-05-01 01:12:19 +0400

    добавил рабочие переключатели даты, + на седня и завтра прогноз но там чет хуйня какая-то не работает епта одно и тоже это мы ослы или кто осел??(((

--- app/src/main/java/com/example/horoscopes/MainActivity.kt
+++ app/src/main/java/com/example/horoscopes/MainActivity.kt
@@ -7,42 +7,53 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.TextView
-import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jsoup.Jsoup
-import org.jsoup.nodes.Document
-import org.jsoup.select.Elements
 import kotlin.concurrent.thread
 
 
 class MainActivity : AppCompatActivity() {
 
     private val URLHoroscope: String = "https://1001goroskop.ru"
+
+    private var forecastToday = ""
+    private var forecastTomorrow = ""
+    private var forecastTodayWeek = ""
+    private var forecastTodayMonth = ""
+
     var Setting: SharedPreferences? = null
     val APP_PREFERENCES: String = "horoscope"
     val APP_PREFERENCES_SELECTED_HOROSCOPE: String = "selectedHoroscope"
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
         Setting = getSharedPreferences(APP_PREFERENCES, Context.MODE_PRIVATE)
 
-        if(Setting!!.contains(APP_PREFERENCES_SELECTED_HOROSCOPE)){
-            if(Setting!!.getString(APP_PREFERENCES_SELECTED_HOROSCOPE, "").equals("aries")) getText(Setting!!.getString(APP_PREFERENCES_SELECTED_HOROSCOPE, "").toString())
-            else if(Setting!!.getString(APP_PREFERENCES_SELECTED_HOROSCOPE, "").equals("taurus")) getText(Setting!!.getString(APP_PREFERENCES_SELECTED_HOROSCOPE, "").toString())
-            else if(Setting!!.getString(APP_PREFERENCES_SELECTED_HOROSCOPE, "").equals("gemini")) getText(Setting!!.getString(APP_PREFERENCES_SELECTED_HOROSCOPE, "").toString())
-            else if(Setting!!.getString(APP_PREFERENCES_SELECTED_HOROSCOPE, "").equals("cancer")) getText(Setting!!.getString(APP_PREFERENCES_SELECTED_HOROSCOPE, "").toString())
-            else if(Setting!!.getString(APP_PREFERENCES_SELECTED_HOROSCOPE, "").equals("leo")) getText(Setting!!.getString(APP_PREFERENCES_SELECTED_HOROSCOPE, "").toString())
-            else if(Setting!!.getString(APP_PREFERENCES_SELECTED_HOROSCOPE, "").equals("virgo")) getText(Setting!!.getString(APP_PREFERENCES_SELECTED_HOROSCOPE, "").toString())
-            else if(Setting!!.getString(APP_PREFERENCES_SELECTED_HOROSCOPE, "").equals("libra")) getText(Setting!!.getString(APP_PREFERENCES_SELECTED_HOROSCOPE, "").toString())
-            else if(Setting!!.getString(APP_PREFERENCES_SELECTED_HOROSCOPE, "").equals("scorpio")) getText(Setting!!.getString(APP_PREFERENCES_SELECTED_HOROSCOPE, "").toString())
-            else if(Setting!!.getString(APP_PREFERENCES_SELECTED_HOROSCOPE, "").equals("sagittarius")) getText(Setting!!.getString(APP_PREFERENCES_SELECTED_HOROSCOPE, "").toString())
-            else if(Setting!!.getString(APP_PREFERENCES_SELECTED_HOROSCOPE, "").equals("capricorn")) getText(Setting!!.getString(APP_PREFERENCES_SELECTED_HOROSCOPE, "").toString())
-            else if(Setting!!.getString(APP_PREFERENCES_SELECTED_HOROSCOPE, "").equals("pisces")) getText(Setting!!.getString(APP_PREFERENCES_SELECTED_HOROSCOPE, "").toString())
-            else tvToday.text = "error text!"
-        }
+        val zodiac = Setting!!.getString(APP_PREFERENCES_SELECTED_HOROSCOPE, "").toString()
+
+//        if(Setting!!.contains(APP_PREFERENCES_SELECTED_HOROSCOPE)){
+//            if(Setting!!.getString(APP_PREFERENCES_SELECTED_HOROSCOPE, "").equals("aries")) getText(Setting!!.getString(APP_PREFERENCES_SELECTED_HOROSCOPE, "").toString())
+//            else if(Setting!!.getString(APP_PREFERENCES_SELECTED_HOROSCOPE, "").equals("taurus")) getText(Setting!!.getString(APP_PREFERENCES_SELECTED_HOROSCOPE, "").toString())
+//            else if(Setting!!.getString(APP_PREFERENCES_SELECTED_HOROSCOPE, "").equals("gemini")) getText(Setting!!.getString(APP_PREFERENCES_SELECTED_HOROSCOPE, "").toString())
+//            else if(Setting!!.getString(APP_PREFERENCES_SELECTED_HOROSCOPE, "").equals("cancer")) getText(Setting!!.getString(APP_PREFERENCES_SELECTED_HOROSCOPE, "").toString())
+//            else if(Setting!!.getString(APP_PREFERENCES_SELECTED_HOROSCOPE, "").equals("leo")) getText(Setting!!.getString(APP_PREFERENCES_SELECTED_HOROSCOPE, "").toString())
+//            else if(Setting!!.getString(APP_PREFERENCES_SELECTED_HOROSCOPE, "").equals("virgo")) getText(Setting!!.getString(APP_PREFERENCES_SELECTED_HOROSCOPE, "").toString())
+//            else if(Setting!!.getString(APP_PREFERENCES_SELECTED_HOROSCOPE, "").equals("libra")) getText(Setting!!.getString(APP_PREFERENCES_SELECTED_HOROSCOPE, "").toString())
+//            else if(Setting!!.getString(APP_PREFERENCES_SELECTED_HOROSCOPE, "").equals("scorpio")) getText(Setting!!.getString(APP_PREFERENCES_SELECTED_HOROSCOPE, "").toString())
+//            else if(Setting!!.getString(APP_PREFERENCES_SELECTED_HOROSCOPE, "").equals("sagittarius")) getText(Setting!!.getString(APP_PREFERENCES_SELECTED_HOROSCOPE, "").toString())
+//            else if(Setting!!.getString(APP_PREFERENCES_SELECTED_HOROSCOPE, "").equals("capricorn")) getText(Setting!!.getString(APP_PREFERENCES_SELECTED_HOROSCOPE, "").toString())
+//            else if(Setting!!.getString(APP_PREFERENCES_SELECTED_HOROSCOPE, "").equals("pisces")) getText(Setting!!.getString(APP_PREFERENCES_SELECTED_HOROSCOPE, "").toString())
+//            else tvMain.text = "error text!"
+//        }
+
+
+        getStartText(zodiac, "today", URLHoroscope)
+
+        getForecastToday(zodiac, "today", URLHoroscope)
+        getForecastTomorrow(zodiac, "tomorrow", URLHoroscope)
     }
 
     private fun getText(zodiac: String) {
@@ -51,13 +62,173 @@ class MainActivity : AppCompatActivity() {
             val textElements = doc.select("div[itemprop=description]")
             val text123 = textElements.select("p").text()
             this@MainActivity.runOnUiThread(java.lang.Runnable {
-                tvToday.text = text123
+                tvMain.text = text123
             })
         }
     }
 
+    // это затычка
+    private fun getStartText(zodiac: String, date: String, url: String) {
+        thread {
+            val doc = Jsoup.connect("$url?kn=$zodiac&kn=$date").get()
+            val textElements = doc.select("div[itemprop=description]")
+            val text123 = textElements.select("p").text()
+            this@MainActivity.runOnUiThread(java.lang.Runnable {
+                tvMain.text = text123
+            })
+        }
+    }
+
+    private fun getForecastToday(zodiac: String, date: String, url: String) {
+        thread {
+            val doc = Jsoup.connect("$url?kn=$zodiac&kn=$date").get()
+            val textElements = doc.select("div[itemprop=description]")
+            val text123 = textElements.select("p").text()
+            this@MainActivity.runOnUiThread(java.lang.Runnable {
+                forecastToday = text123
+            })
+        }
+    }
+
+    private fun getForecastTomorrow(zodiac: String, date: String, url: String) {
+        thread {
+            val doc = Jsoup.connect("$url?kn=$zodiac&kn=$date").get()
+            val textElements = doc.select("div[itemprop=description]")
+            val text123 = textElements.select("p").text()
+            this@MainActivity.runOnUiThread(java.lang.Runnable {
+                forecastTomorrow = text123
+            })
+        }
+    }
+
+    fun watchToday(view: View) {
+        switchColorsForBtnToday()
+        tvMain.text = forecastToday
+    }
+
     fun watchTommorow(view: View) {
+        switchColorsForBtnTommorow()
+        tvMain.text = forecastTomorrow
+    }
+
+    fun watchWeek(view: View) {
+        switchColorsForBtnWeek()
+//        getForecast("aries", "week", "https://1001goroskop.ru")
+    }
+
+    fun watchMonth(view: View) {
+        switchColorsForBtnMonth()
+    }
+
+    fun openMenu(view: View) {
+        //TODO тут короче сделать шобы данные стирались и мы заново выбирали знак зодиака
+    }
+
+    private fun switchColorsForBtnToday() {
+        btnToday.setBackgroundResource(R.drawable.button)
+        btnToday.setTextColor(Color.WHITE)
+
+        btnTodayDown.setBackgroundResource(R.drawable.button)
+        btnTodayDown.setTextColor(Color.WHITE)
+        // остальные белыми
+        btnTommorow.setBackgroundResource(R.color.white_true)
+        btnTommorow.setTextColor(getColor(R.color.blueApp))
+
+        btnTommorowDown.setBackgroundResource(R.color.white_true)
+        btnTommorowDown.setTextColor(getColor(R.color.blueApp))
+        //
+        btnWeek.setBackgroundResource(R.color.white_true)
+        btnWeek.setTextColor(getColor(R.color.blueApp))
+
+        btnWeekDown.setBackgroundResource(R.color.white_true)
+        btnWeekDown.setTextColor(getColor(R.color.blueApp))
+        //
+        btnMonth.setBackgroundResource(R.color.white_true)
+        btnMonth.setTextColor(getColor(R.color.blueApp))
+
+        btnMonthDown.setBackgroundResource(R.color.white_true)
+        btnMonthDown.setTextColor(getColor(R.color.blueApp))
+    }
+
+    private fun switchColorsForBtnTommorow() {
         btnTommorow.setBackgroundResource(R.drawable.button)
         btnTommorow.setTextColor(Color.WHITE)
+
+        btnTommorowDown.setBackgroundResource(R.drawable.button)
+        btnTommorowDown.setTextColor(Color.WHITE)
+
+        // остальные белыми
+        btnToday.setBackgroundResource(R.color.white_true)
+        btnToday.setTextColor(getColor(R.color.blueApp))
+
+        btnTodayDown.setBackgroundResource(R.color.white_true)
+        btnTodayDown.setTextColor(getColor(R.color.blueApp))
+        //
+        btnWeek.setBackgroundResource(R.color.white_true)
+        btnWeek.setTextColor(getColor(R.color.blueApp))
+
+        btnWeekDown.setBackgroundResource(R.color.white_true)
+        btnWeekDown.setTextColor(getColor(R.color.blueApp))
+        //
+        btnMonth.setBackgroundResource(R.color.white_true)
+        btnMonth.setTextColor(getColor(R.color.blueApp))
+
+        btnMonthDown.setBackgroundResource(R.color.white_true)
+        btnMonthDown.setTextColor(getColor(R.color.blueApp))
     }
+
+    private fun switchColorsForBtnWeek() {
+        btnWeek.setBackgroundResource(R.drawable.button)
+        btnWeek.setTextColor(Color.WHITE)
+
+        btnWeekDown.setBackgroundResource(R.drawable.button)
+        btnWeekDown.setTextColor(Color.WHITE)
+
+        // остальные белыми
+        btnToday.setBackgroundResource(R.color.white_true)
+        btnToday.setTextColor(getColor(R.color.blueApp))
+
+        btnTodayDown.setBackgroundResource(R.color.white_true)
+        btnTodayDown.setTextColor(getColor(R.color.blueApp))
+        //
+        btnTommorow.setBackgroundResource(R.color.white_true)
+        btnTommorow.setTextColor(getColor(R.color.blueApp))
+
+        btnTommorowDown.setBackgroundResource(R.color.white_true)
+        btnTommorowDown.setTextColor(getColor(R.color.blueApp))
+        //
+        btnMonth.setBackgroundResource(R.color.white_true)
+        btnMonth.setTextColor(getColor(R.color.blueApp))
+
+        btnMonthDown.setBackgroundResource(R.color.white_true)
+        btnMonthDown.setTextColor(getColor(R.color.blueApp))
+    }
+
+    private fun switchColorsForBtnMonth() {
+        btnMonth.setBackgroundResource(R.drawable.button)
+        btnMonth.setTextColor(Color.WHITE)
+
+        btnMonthDown.setBackgroundResource(R.drawable.button)
+        btnMonthDown.setTextColor(Color.WHITE)
+
+        // остальные белыми
+        btnToday.setBackgroundResource(R.color.white_true)
+        btnToday.setTextColor(getColor(R.color.blueApp))
+
+        btnTodayDown.setBackgroundResource(R.color.white_true)
+        btnTodayDown.setTextColor(getColor(R.color.blueApp))
+        //
+        btnTommorow.setBackgroundResource(R.color.white_true)
+        btnTommorow.setTextColor(getColor(R.color.blueApp))
+
+        btnTommorowDown.setBackgroundResource(R.color.white_true)
+        btnTommorowDown.setTextColor(getColor(R.color.blueApp))
+        //
+        btnWeek.setBackgroundResource(R.color.white_true)
+        btnWeek.setTextColor(getColor(R.color.blueApp))
+
+        btnWeekDown.setBackgroundResource(R.color.white_true)
+        btnWeekDown.setTextColor(getColor(R.color.blueApp))
+    }
+
 }
