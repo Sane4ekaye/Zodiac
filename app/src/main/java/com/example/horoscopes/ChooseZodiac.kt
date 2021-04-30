65606c97a7093fa16b20659510da19044b29666e
MistiJI <spam-mistbtv@yandex.ru>
2021-04-30 22:00:45 +0400

    a123

--- app/src/main/java/com/example/horoscopes/ChooseZodiac.kt
+++ app/src/main/java/com/example/horoscopes/ChooseZodiac.kt
@@ -1,5 +1,6 @@
 package com.example.horoscopes
 
+import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
@@ -12,15 +13,28 @@ class ChooseZodiac : AppCompatActivity() {
 
     var btnOven: ImageView? = null
     val APP_PREFERENCES: String = "horoscope"
+    val APP_PREFERENCES_SELECTED_HOROSCOPE: String = "selectedHoroscope"
+    var isSelectedHoroscope: Boolean? = null
     var Setting: SharedPreferences? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_choose_zodiac)
+        Setting = getSharedPreferences(APP_PREFERENCES, Context.MODE_PRIVATE)
         btnOven = findViewById(R.id.buttonOven)
-        btnOven?.setOnClickListener{
-            var intent = Intent(this@ChooseZodiac, MainActivity::class.java)
+
+        if(Setting!!.contains(APP_PREFERENCES_SELECTED_HOROSCOPE)) {
+            val intent = Intent(this@ChooseZodiac, MainActivity::class.java)
             startActivity(intent)
+        } else {
+            isSelectedHoroscope = false
+        }
+
+        btnOven?.setOnClickListener{
+            if(isSelectedHoroscope == false){
+                val intent = Intent(this@ChooseZodiac, MainActivity::class.java)
+                startActivity(intent)
+            }
         }
     }
 }
--- app/src/main/java/com/example/horoscopes/MainActivity.kt
+++ app/src/main/java/com/example/horoscopes/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.horoscopes
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 import org.jsoup.select.Elements
@@ -12,12 +13,9 @@ import kotlin.concurrent.thread
 class MainActivity : AppCompatActivity() {
 
     private val URLHoroscope: String = "https://1001goroskop.ru"
-
-    var textMain: TextView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        textMain = findViewById(R.id.textMain)
         getText("gemini")
     }
     private fun getText(zodiac: String){
@@ -26,7 +24,7 @@ class MainActivity : AppCompatActivity() {
             val textElements = doc.select("div[itemprop=description]")
             val text123 = textElements.select("p").text()
             //val textEnd = doc.getElementById("description")
-            textMain!!.text = text123
+            tvToday!!.text = text123
         }
     }
 }
