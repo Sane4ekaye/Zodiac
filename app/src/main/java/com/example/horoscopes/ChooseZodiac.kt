44c73f3af2e4758c65649a4926700e503589f9e3
MistiJI <spam-mistbtv@yandex.ru>
2021-04-30 23:32:42 +0400

    вот где мой сон

--- app/src/main/java/com/example/horoscopes/ChooseZodiac.kt
+++ app/src/main/java/com/example/horoscopes/ChooseZodiac.kt
@@ -8,10 +8,10 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_choose_zodiac.*
 
 class ChooseZodiac : AppCompatActivity() {
 
-    var btnOven: ImageView? = null
     val APP_PREFERENCES: String = "horoscope"
     val APP_PREFERENCES_SELECTED_HOROSCOPE: String = "selectedHoroscope"
     var isSelectedHoroscope: Boolean? = null
@@ -21,20 +21,90 @@ class ChooseZodiac : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_choose_zodiac)
         Setting = getSharedPreferences(APP_PREFERENCES, Context.MODE_PRIVATE)
-        btnOven = findViewById(R.id.buttonOven)
 
         if(Setting!!.contains(APP_PREFERENCES_SELECTED_HOROSCOPE)) {
             val intent = Intent(this@ChooseZodiac, MainActivity::class.java)
             startActivity(intent)
+            finish()
         } else {
             isSelectedHoroscope = false
         }
 
-        btnOven?.setOnClickListener{
+
+        // ============ Buttons ============ //
+
+        buttonOven?.setOnClickListener{
             if(isSelectedHoroscope == false){
-                val intent = Intent(this@ChooseZodiac, MainActivity::class.java)
-                startActivity(intent)
+                putSharedPreference("aries")
             }
         }
+        buttonTaurus?.setOnClickListener{
+            if(isSelectedHoroscope == false){
+                putSharedPreference("taurus")
+            }
+        }
+        buttonTwins?.setOnClickListener{
+            if(isSelectedHoroscope == false){
+                putSharedPreference("gemini")
+            }
+        }
+        buttonCancer?.setOnClickListener{
+            if(isSelectedHoroscope == false){
+                putSharedPreference("cancer")
+            }
+        }
+        buttonLion?.setOnClickListener{
+            if(isSelectedHoroscope == false){
+                putSharedPreference("leo")
+            }
+        }
+        buttonVirgin?.setOnClickListener{
+            if(isSelectedHoroscope == false){
+                putSharedPreference("virgo")
+            }
+        }
+        buttonScales?.setOnClickListener{
+            if(isSelectedHoroscope == false){
+                putSharedPreference("libra")
+            }
+        }
+        buttonScorpio?.setOnClickListener{
+            if(isSelectedHoroscope == false){
+                putSharedPreference("scorpio")
+            }
+        }
+        buttonSagittarius?.setOnClickListener{
+            if(isSelectedHoroscope == false){
+                putSharedPreference("sagittarius")
+            }
+        }
+        buttonCapricorn?.setOnClickListener{
+            if(isSelectedHoroscope == false){
+                putSharedPreference("capricorn")
+            }
+        }
+        buttonAquarius?.setOnClickListener{
+            if(isSelectedHoroscope == false){
+                putSharedPreference("aquarius")
+            }
+        }
+        buttonFish?.setOnClickListener{
+            if(isSelectedHoroscope == false){
+                putSharedPreference("pisces")
+            }
+        }
+
+        // ================================ //
+    }
+
+    fun putSharedPreference(horoscope: String){
+        var editor: SharedPreferences.Editor = Setting!!.edit()
+        editor.putString(APP_PREFERENCES_SELECTED_HOROSCOPE, horoscope)
+        editor.apply()
+        addIntent()
+    }
+    fun addIntent(){
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/example/horoscopes/MainActivity.kt
+++ app/src/main/java/com/example/horoscopes/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.horoscopes
 
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
@@ -15,16 +19,35 @@ import kotlin.concurrent.thread
 class MainActivity : AppCompatActivity() {
 
     private val URLHoroscope: String = "https://1001goroskop.ru"
+    var Setting: SharedPreferences? = null
+    val APP_PREFERENCES: String = "horoscope"
+    val APP_PREFERENCES_SELECTED_HOROSCOPE: String = "selectedHoroscope"
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        getText("week")
+        Setting = getSharedPreferences(APP_PREFERENCES, Context.MODE_PRIVATE)
+
+        if(Setting!!.contains(APP_PREFERENCES_SELECTED_HOROSCOPE)){
+            if(Setting!!.getString(APP_PREFERENCES_SELECTED_HOROSCOPE, "").equals("aries")) getText(Setting!!.getString(APP_PREFERENCES_SELECTED_HOROSCOPE, "").toString())
+            else if(Setting!!.getString(APP_PREFERENCES_SELECTED_HOROSCOPE, "").equals("taurus")) getText(Setting!!.getString(APP_PREFERENCES_SELECTED_HOROSCOPE, "").toString())
+            else if(Setting!!.getString(APP_PREFERENCES_SELECTED_HOROSCOPE, "").equals("gemini")) getText(Setting!!.getString(APP_PREFERENCES_SELECTED_HOROSCOPE, "").toString())
+            else if(Setting!!.getString(APP_PREFERENCES_SELECTED_HOROSCOPE, "").equals("cancer")) getText(Setting!!.getString(APP_PREFERENCES_SELECTED_HOROSCOPE, "").toString())
+            else if(Setting!!.getString(APP_PREFERENCES_SELECTED_HOROSCOPE, "").equals("leo")) getText(Setting!!.getString(APP_PREFERENCES_SELECTED_HOROSCOPE, "").toString())
+            else if(Setting!!.getString(APP_PREFERENCES_SELECTED_HOROSCOPE, "").equals("virgo")) getText(Setting!!.getString(APP_PREFERENCES_SELECTED_HOROSCOPE, "").toString())
+            else if(Setting!!.getString(APP_PREFERENCES_SELECTED_HOROSCOPE, "").equals("libra")) getText(Setting!!.getString(APP_PREFERENCES_SELECTED_HOROSCOPE, "").toString())
+            else if(Setting!!.getString(APP_PREFERENCES_SELECTED_HOROSCOPE, "").equals("scorpio")) getText(Setting!!.getString(APP_PREFERENCES_SELECTED_HOROSCOPE, "").toString())
+            else if(Setting!!.getString(APP_PREFERENCES_SELECTED_HOROSCOPE, "").equals("sagittarius")) getText(Setting!!.getString(APP_PREFERENCES_SELECTED_HOROSCOPE, "").toString())
+            else if(Setting!!.getString(APP_PREFERENCES_SELECTED_HOROSCOPE, "").equals("capricorn")) getText(Setting!!.getString(APP_PREFERENCES_SELECTED_HOROSCOPE, "").toString())
+            else if(Setting!!.getString(APP_PREFERENCES_SELECTED_HOROSCOPE, "").equals("pisces")) getText(Setting!!.getString(APP_PREFERENCES_SELECTED_HOROSCOPE, "").toString())
+            else tvToday.text = "error text!"
+        }
     }
 
     private fun getText(zodiac: String) {
         thread {
-            val doc = Jsoup.connect("$URLHoroscope?kn=$zodiac").get()
+            val doc = Jsoup.connect("$URLHoroscope?znak=$zodiac").get()
             val textElements = doc.select("div[itemprop=description]")
             val text123 = textElements.select("p").text()
             this@MainActivity.runOnUiThread(java.lang.Runnable {
