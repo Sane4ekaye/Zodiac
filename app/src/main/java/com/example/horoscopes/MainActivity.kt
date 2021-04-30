786e51324fb15db5e07f6d20b667d657b128ef0f
chikibambsanya <BKyPeHblu456@yandex.ru>
2021-04-30 22:36:22 +0400

    dfsfgasd

--- app/src/main/java/com/example/horoscopes/MainActivity.kt
+++ app/src/main/java/com/example/horoscopes/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.horoscopes
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jsoup.Jsoup
@@ -13,18 +15,26 @@ import kotlin.concurrent.thread
 class MainActivity : AppCompatActivity() {
 
     private val URLHoroscope: String = "https://1001goroskop.ru"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        getText("gemini")
+        getText("week")
     }
-    private fun getText(zodiac: String){
+
+    private fun getText(zodiac: String) {
         thread {
-            val doc = Jsoup.connect("$URLHoroscope?znak=$zodiac").get()
+            val doc = Jsoup.connect("$URLHoroscope?kn=$zodiac").get()
             val textElements = doc.select("div[itemprop=description]")
             val text123 = textElements.select("p").text()
-            //val textEnd = doc.getElementById("description")
-            tvToday!!.text = text123
+            this@MainActivity.runOnUiThread(java.lang.Runnable {
+                tvToday.text = text123
+            })
         }
+    }
+
+    fun watchTommorow(view: View) {
+        btnTommorow.setBackgroundResource(R.drawable.button)
+        btnTommorow.setTextColor(Color.WHITE)
     }
 }
